1ed9a6981c29910d3a9e0ad7a8de18905f2d17b1
Jeyakumar <jeyakumar1442@gmail.com>
2025-11-05 22:05:54 +0530

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+
+
+import authRoutes from './routes/authRoutes.js';
+import { connectDB } from './lib/db.js';
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());    
+
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectDB();
+});
