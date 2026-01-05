9197704d095ab7904d36be4e3c7bafc3dffe5957
Jeyakumar <jeyakumar1442@gmail.com>
2026-01-05 15:10:16 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,12 +28,13 @@ app.use('/api/messages', messageRoutes);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-    app.get('/*', (req, res) => {
+    app.use((req, res) => {
         res.sendFile(
             path.resolve(__dirname, '../frontend/dist/index.html')
         );
     });
 }
+
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
