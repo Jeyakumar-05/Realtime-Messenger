6c5eeb9c37a28d0fd7a64e85d60c12ba9024a702
Jeyakumar <jeyakumar1442@gmail.com>
2026-01-05 14:44:29 +0530

    deployment done

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 
+import path from 'path';
 import authRoutes from './routes/authRoutes.js';
 import messageRoutes from './routes/messageRoutes.js';
 import { connectDB } from './lib/db.js';
@@ -10,6 +11,7 @@ import { app, server } from './lib/socket.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
@@ -20,9 +22,13 @@ app.use(cors({
     credentials: true
 }));
 
-
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html')));
+}
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
